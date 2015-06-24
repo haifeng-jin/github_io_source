fd24c7872372e8dd07a69143f87cd28fe4edf092
jhfjhfj1 <jhfjhfj1@gmail.com>
2015-06-24 23:25:36 +0800

    refraction

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+cp -r common home/_includes/
+cp -r common codeforces/_includes/
+cp -r common chinese/_includes/
 jekyll build -s home/ -d site/
 jekyll build -s codeforces/ -d site/codeforces/
 jekyll build -s chinese/ -d site/chinese/
