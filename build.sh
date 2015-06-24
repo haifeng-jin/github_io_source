367c7d4f5166edea57ce73125fffa8d9cc7a08ed
jhfjhfj1 <jhfjhfj1@gmail.com>
2015-06-25 00:15:36 +0800

    technology

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 cp -r common home/_includes/
 cp -r common codeforces/_includes/
 cp -r common chinese/_includes/
+cp -r common technology/_includes/
 jekyll build -s home/ -d site/
 jekyll build -s codeforces/ -d site/codeforces/
 jekyll build -s chinese/ -d site/chinese/
+jekyll build -s technology/ -d site/technology/
