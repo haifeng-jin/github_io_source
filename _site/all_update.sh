254934c64ca8dda0fec98483f5e3365028900103
Haifeng Jin <haifeng.jin@hulu.com>
2016-08-06 22:09:56 -0700

    rm

--- _site/all_update.sh
+++
@@ -1,3 +0,0 @@
-sh update.sh $1
-cd ../jhfjhfj1.github.io
-sh update.sh $1
--- _site/update.sh
+++
@@ -1,5 +0,0 @@
-sh clean.sh
-git add --all
-git commit -m "unknown"
-git push origin master
-cp -r _site/* ../jhfjhfj1.github.io/
