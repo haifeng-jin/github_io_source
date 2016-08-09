526892b07bd822db7ba49df6ecc372979a929178
Haifeng Jin <haifeng.jin@hulu.com>
2016-08-08 17:11:15 -0700

    re

--- all_update.sh
+++ all_update.sh
@@ -1,3 +1,7 @@
+sh clean.sh
+jekyll build
+rm -r ../jhfjhfj1.github.io/*
+cp -r _site/* ../jhfjhfj1.github.io/
 sh update.sh $1
 cd ../jhfjhfj1.github.io
 sh update.sh $1
--- update.sh
+++ update.sh
@@ -1,7 +1,3 @@
-sh clean.sh
-jekyll build
 git add --all
 git commit -m $1
 git push origin master
-rm -r ../jhfjhfj1.github.io/*
-cp -r _site/* ../jhfjhfj1.github.io/
