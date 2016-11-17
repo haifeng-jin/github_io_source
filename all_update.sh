8d04c7cb8ef3d8525f89f3cc1b147a22f430bb43
Haifeng Jin <jhfjhfj1@gmail.com>
2016-11-16 21:49:15 -0600

    tracker

--- all_update.sh
+++ all_update.sh
@@ -1,9 +1,10 @@
 sh clean.sh
 jekyll build
+cd ../jhfjhfj1.github.io
+git pull
+cd ../github_io_source
 rm -r ../jhfjhfj1.github.io/*
 cp -r _site/* ../jhfjhfj1.github.io/
 sh update.sh $1
 cd ../jhfjhfj1.github.io
-git add --all
-git commit -m $1
-git push origin master -f
+sh update.sh $1
