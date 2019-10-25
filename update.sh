97d34fc0cd57165a84c1f9ec6285442fff94bd33
Haifeng Jin <jhfjhfj1@gmail.com>
2019-10-25 15:50:36 -0500

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 jekyll build
+git checkout -b build
 git add build
-git commit -m "update"
+git commit -m "build"
 git subtree split --prefix build -b gh-pages
 git push -f origin gh-pages:gh-pages
+git checkout master
 git branch -D gh-pages
+git branch -D build
 rm -rf build
 
