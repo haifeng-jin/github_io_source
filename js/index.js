4be492f0838b8342bba95fa0e20e2c59db0a7b94
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2021-12-28 18:14:09 -0800

    add messages

--- js/index.js
+++ js/index.js
@@ -27,3 +27,62 @@ function alert(message, duration) {
     alertPlaceholder.innerHTML = ''
   }, duration);
 }
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+messages = [
+    // Knowledges
+    "Moore's law is the observation that the number of transistors in a dense integrated circuit doubles about every two years.",
+    "Andy and Bill's law is a statement that new software will always consume any increase in computing power that new hardware can provide.",
+    "Conway's law: organizations design systems mirror their own communication structure.",
+    // Thoughts
+    "Keep unit tests simple and understandable at a glance. Otherwise, the testing code itself is prune to bugs. When writing code, we always want to reduce code duplication. However, writing tests is an exception, because reducing code duplication to the extreme may harm the tests' readability. In summary, we need to balance between better readability and less duplication.",
+    "We should use the Semantic Versioning (semver.org) system for software's version numbers. It is a common way to communicate with the users about how much was the software changed comparing to its predecessors.",
+    // Experiences
+    "I learned programming during my high school study to participate in National Olympiad in Informatics in Provinces (NOIP).",
+    "I studied as an undergrad in computer science at Beijing Forestry University (BFU).",
+    "I completed my Master's Degree in computer science at Beijing University of Posts and Telecommunications (BUPT).",
+    "I completed my Ph.D. in computer science at Texas A&M University (TAMU).",
+    "I did three internships on Keras team at Google, which accumulates up to a year, before I officially joined as a full-time employee.",
+    "During my undergrad, I was so passionate about ACM-ICPC (International Collegiate Programming Contest), which was not so popular in my university (BFU) at the time. I took the training in Peking University and China University of Geosciences, and bring the things I learned back to BFU. We started a student organization to help people better prepare for ACM-ICPC. Finally, we won the first ACM-ICPC medal in BFU's history. Though, it was a bronze medal. LOL",
+];
+shuffle(messages);
+counter = 0;
+
+function refresh_message() {
+    message = messages[counter];
+    counter = (counter + 1) % messages.length;
+    transition_time = 500;
+    $('#message-text').text(message).fadeIn(transition_time);
+    timeout = message.length * 100;
+    window.setTimeout(function() {$('#message-text').text(message).fadeOut(transition_time);}, timeout + transition_time);
+    window.setTimeout(refresh_message, timeout + transition_time * 2);
+    $('#progress').css('width', '100%');
+    $('#progress').animate({width: '0%'}, timeout + transition_time);
+}
+
+window.onload = (event) => {
+    refresh_message()
+};
