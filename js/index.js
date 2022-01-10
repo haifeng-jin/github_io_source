d6eb94a0e53a32f26a22c07add71b1b5b7e2eba6
Haifeng Jin <jhfjhfj1@gmail.com>
2022-01-09 18:39:17 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -76,6 +76,7 @@ messages = [
     "I developed AutoKeras during my Ph.D. study and started the collaboration with Keras team from Google.",
     "During my internships at Google, I was involved in the initial design and development of the KerasTuner project.",
     "During my undergrad, I was so passionate about ACM-ICPC (International Collegiate Programming Contest), which was not so popular in my university (BFU) at the time. I took the training at Peking University and China University of Geosciences, and bring the things I learned back to BFU. We started a student organization to help people better prepare for ACM-ICPC. Finally, we won the first ACM-ICPC medal in BFU's history. Though, it was a bronze medal. LOL",
+    "As I join the Keras team, I started on a project to explain the Keras codebase to ramp up my work, which you can find it <a href='https://haifeng-jin.github.io/keras-source/'>here</a>.",
 ];
 shuffle(messages);
 counter = 0;
